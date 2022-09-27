27541ea609a05b62f922f0fd9e971b25a03b9218
jd602 <juand.sarazan@uqvirtual.edu.co>
2022-09-27 01:05:31 -0500

    first commit

---
+++ backend/src/main/java/co/edu/uniquindio/unicine/BackendApplication.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.unicine;
+
+import org.springframework.boot.SpringApplication;
+
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
